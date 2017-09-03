0e989b68a1b9684f106e9ba0027461e9bb25b9be
Thijs Cadier <thijs@appsignal.com>
2017-09-03 22:36:14 +0200

    Improve ffi spec a bit

--- spec/rdkafka/ffi_spec.rb
+++ spec/rdkafka/ffi_spec.rb
@@ -1,7 +1,13 @@
 require "spec_helper"
 
 describe Rdkafka::FFI do
+  it "should load librdkafka" do
+    expect(Rdkafka::FFI.ffi_libraries.map(&:name).first).to include "librdkafka"
+  end
+
   it "should successfully call librdkafka" do
-    Rdkafka::FFI.rd_kafka_conf_new
+    expect {
+      Rdkafka::FFI.rd_kafka_conf_new
+    }.not_to raise_error
   end
 end

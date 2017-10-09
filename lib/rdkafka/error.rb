4d8008cb3546a8bcd6e9017f1aefe514eda15ece
Thijs Cadier <thijs@appsignal.com>
2017-10-09 11:13:14 -0400

    Document RdkafkaError

--- lib/rdkafka/error.rb
+++ lib/rdkafka/error.rb
@@ -1,12 +1,18 @@
 module Rdkafka
+  # Error returned by the underlying rdkafka library.
   class RdkafkaError < RuntimeError
+    # The underlying raw error response
+    # @return [Integer]
     attr_reader :rdkafka_response
 
+    # @private
     def initialize(response)
       raise TypeError.new("Response has to be an integer") unless response.is_a? Integer
       @rdkafka_response = response
     end
 
+    # This error's code, for example `:partition_eof`, `:msg_size_too_large`.
+    # @return [Symbol]
     def code
       code = Rdkafka::FFI.rd_kafka_err2name(@rdkafka_response).downcase
       if code[0] == "_"
@@ -16,10 +22,14 @@ module Rdkafka
       end
     end
 
+    # Human readable representation of this error.
+    # @return [String]
     def to_s
       "#{Rdkafka::FFI.rd_kafka_err2str(@rdkafka_response)} (#{code})"
     end
 
+    # Whether this error indicates the partition is EOF.
+    # @return [Boolean]
     def is_partition_eof?
       code == :partition_eof
     end

d94856e2607d19a228f05ecaa8964da66ab63e23
Thijs Cadier <thijs@appsignal.com>
2017-10-23 14:25:15 +0200

    Make clear that adding a topic supports ranges

--- spec/rdkafka/consumer/topic_partition_list_spec.rb
+++ spec/rdkafka/consumer/topic_partition_list_spec.rb
@@ -37,7 +37,32 @@ describe Rdkafka::Consumer::TopicPartitionList do
     })
   end
 
-  it "should create a new list and add assigned topics" do
+  it "should create a new list and add assigned topics as a range" do
+    list = Rdkafka::Consumer::TopicPartitionList.new
+
+    expect(list.count).to eq 0
+    expect(list.empty?).to be true
+
+    list.add_topic("topic1", (0..2))
+    list.add_topic("topic2", (0..1))
+
+    expect(list.count).to eq 5
+    expect(list.empty?).to be false
+
+    hash = list.to_h
+    expect(hash.count).to eq 2
+    expect(hash["topic1"]).to eq([
+      Rdkafka::Consumer::Partition.new(0, -1001),
+      Rdkafka::Consumer::Partition.new(1, -1001),
+      Rdkafka::Consumer::Partition.new(2, -1001)
+    ])
+    expect(hash["topic2"]).to eq([
+      Rdkafka::Consumer::Partition.new(0, -1001),
+      Rdkafka::Consumer::Partition.new(1, -1001)
+    ])
+  end
+
+  it "should create a new list and add assigned topics as an array" do
     list = Rdkafka::Consumer::TopicPartitionList.new
 
     expect(list.count).to eq 0

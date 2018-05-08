9609363b7a2e4856f9330135ecfb604fe9272036
Thijs Cadier <thijs@appsignal.com>
2018-05-08 09:47:03 +0200

    Use Time for timestamps
    When producing a messages an integer or Time object is accepted. When
    consuming a messages a Time is always returned.

--- lib/rdkafka/consumer/message.rb
+++ lib/rdkafka/consumer/message.rb
@@ -23,23 +23,37 @@ module Rdkafka
       attr_reader :offset
 
       # This message's timestamp, if provided by the broker
-      # @return [Integer, nil]
+      # @return [Time, nil]
       attr_reader :timestamp
 
       # @private
       def initialize(native_message)
+        # Set topic
         unless native_message[:rkt].null?
           @topic = Rdkafka::Bindings.rd_kafka_topic_name(native_message[:rkt])
         end
+        # Set partition
         @partition = native_message[:partition]
+        # Set payload
         unless native_message[:payload].null?
           @payload = native_message[:payload].read_string(native_message[:len])
         end
+        # Set key
         unless native_message[:key].null?
           @key = native_message[:key].read_string(native_message[:key_len])
         end
+        # Set offset
         @offset = native_message[:offset]
-        @timestamp = Rdkafka::Bindings.rd_kafka_message_timestamp(native_message, nil)
+        # Set timestamp
+        raw_timestamp = Rdkafka::Bindings.rd_kafka_message_timestamp(native_message, nil)
+        @timestamp = if raw_timestamp && raw_timestamp > -1
+                       # Calculate seconds and microseconds
+                       seconds = raw_timestamp / 1000
+                       milliseconds = (raw_timestamp - seconds * 1000) * 1000
+                       Time.at(seconds, milliseconds)
+                     else
+                       nil
+                     end
       end
 
       # Human readable representation of this message.

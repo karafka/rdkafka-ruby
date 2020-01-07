4e51391072db99f6bc16cc025ff6344033922dd0
Abdul Chaudhry <abdollar@yahoo.com>
2020-01-07 15:15:23 -0800

    CD-183087 master: upgrade librdkafka to release 1.2.2-RC1

--- lib/rdkafka/version.rb
+++ lib/rdkafka/version.rb
@@ -1,7 +1,7 @@
 module Rdkafka
   VERSION = '0.6.0-c4'.freeze
-  LIBRDKAFKA_VERSION = '1.3.0'.freeze
+  LIBRDKAFKA_VERSION = '1.2.2-RC1'.freeze
   # SHA256 checksum for tar.gz can be found here https://github.com/edenhill/librdkafka/releases
   LIBRDKAFKA_SOURCE_SHA256 =
-    '465cab533ebc5b9ca8d97c90ab69e0093460665ebaf38623209cf343653c76d2'.freeze
+    'd44046cd6cf03fbcdf3f776feddc9c972e67a5dd28ac7ebabed43dbf0b2379ca'.freeze
 end

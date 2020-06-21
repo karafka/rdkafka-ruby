22d32e59ddc2d7e5b1e7072a19b3bd3d1e32caee
Geoff Thé <geoff@stitchfix.com>
2020-06-21 11:00:28 -0700

    Add BackgroundEventCallback

--- lib/rdkafka/bindings.rb
+++ lib/rdkafka/bindings.rb
@@ -286,6 +286,46 @@ module Rdkafka
     attach_function :rd_kafka_topic_result_error, [:pointer], :int32
     attach_function :rd_kafka_topic_result_error_string, [:pointer], :pointer
     attach_function :rd_kafka_topic_result_name, [:pointer], :pointer
+
+    class TopicResult
+      attr_reader :result_error, :error_string, :result_name
+
+      def initialize(pointer)
+        @result_error = Rdkafka::Bindings.rd_kafka_topic_result_error(pointer)
+        @error_string = Rdkafka::Bindings.rd_kafka_topic_result_error_string(pointer)
+        @result_name = Rdkafka::Bindings.rd_kafka_topic_result_name(pointer)
+      end
+    end
+
+    BackgroundEventCallback = FFI::Function.new(
+        :void, [:pointer, :pointer, :pointer]
+    ) do |client_ptr, event_ptr, opaque_ptr|
+
+      return if Rdkafka::Bindings.rd_kafka_event_type(event_ptr) != RD_KAFKA_EVENT_CREATETOPICS_RESULT
+
+      create_topics_result = Rdkafka::Bindings.rd_kafka_event_CreateTopics_result(event_ptr)
+
+      # Get the number of topic results
+      pointer_to_size_t = FFI::MemoryPointer.new(:int32)
+      topic_results = Rdkafka::Bindings.rd_kafka_CreateTopics_result_topics(create_topics_result, pointer_to_size_t)
+      topic_result_count = pointer_to_size_t.read(:int32)
+
+      topic_results = (1..topic_result_count).map do |index|
+        pointer = (topic_results + (index - 1)).read(:pointer)
+        TopicResult.new(pointer)
+      end
+
+      create_topic_handle_ptr = Rdkafka::Bindings.rd_kafka_event_opaque(event_ptr)
+
+      if create_topic_handle = Rdkafka::Admin::CreateTopicHandle.remove(create_topic_handle_ptr.address)
+        create_topic_handle[:pending] = false
+        # TODO GT: support more than one topic creation at a time?
+        create_topic_handle[:response] = topic_results[0].result_error
+        create_topic_handle[:error_string] = topic_results[0].error_string
+        create_topic_handle[:result_name] = topic_results[0].result_name
+      end
+    end
+
     DeliveryCallback = FFI::Function.new(
       :void, [:pointer, :pointer, :pointer]
     ) do |client_ptr, message_ptr, opaque_ptr|

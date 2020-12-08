7d41fa6cd3d7d1801d7fa148f94ad77373cf0cba
Steven Diviney <sdiviney@zendesk.com>
2020-12-08 08:56:10 -0800

    Switch to murmur2 partitioning

--- lib/rdkafka/bindings.rb
+++ lib/rdkafka/bindings.rb
@@ -236,13 +236,14 @@ module Rdkafka
 
     # Partitioner
     attach_function :rd_kafka_msg_partitioner_consistent_random, [:pointer, :pointer, :size_t, :int32, :pointer, :pointer], :int32
+    attach_function :rd_kafka_msg_partitioner_murmur2_random, [:pointer, :pointer, :size_t, :int32, :pointer, :pointer], :int32
 
     def self.partitioner(str, partition_count)
       # Return RD_KAFKA_PARTITION_UA(unassigned partition) when partition count is nil/zero.
       return -1 unless partition_count&.nonzero?
 
       str_ptr = FFI::MemoryPointer.from_string(str)
-      rd_kafka_msg_partitioner_consistent_random(nil, str_ptr, str.size, partition_count, nil, nil)
+      rd_kafka_msg_partitioner_murmur2_random(nil, str_ptr, str.size, partition_count, nil, nil)
     end
 
     # Create Topics

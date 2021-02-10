1a30223a895ee5b45798d75205e7704d0efc2746
Johnny Lai <johnny.lai@me.com>
2021-02-10 10:33:46 -0800

    Update version.rb

--- lib/rdkafka/version.rb
+++ lib/rdkafka/version.rb
@@ -1,5 +1,5 @@
 module Rdkafka
-  VERSION = '0.6.0-c8'.freeze
+  VERSION = '0.6.0-c9'.freeze
   LIBRDKAFKA_VERSION = '1.2.2'.freeze
   # SHA256 checksum for tar.gz can be found here https://github.com/edenhill/librdkafka/releases
   LIBRDKAFKA_SOURCE_SHA256 =

b51dc0fdaac1fe18cc1280d6accbd1532790dff2
João Vortmann <jvortmann@pm.me>
2021-10-26 23:37:26 -0300

    Move Producer Client up and inject it
    This also explicitly define a finalizer method to return a `proc`
    instead of making the client itself callable.
    
    ---
    
    Changelog: refactor
    Refactoring: moveClass

--- lib/rdkafka/producer.rb
+++ lib/rdkafka/producer.rb
@@ -10,52 +10,11 @@ module Rdkafka
     attr_reader :delivery_callback
 
     # @private
-    def initialize(native_kafka)
-      @client = ProducerClient.new(native_kafka)
+    def initialize(client)
+      @client = client
 
       # Makes sure, that the producer gets closed before it gets GCed by Ruby
-      ObjectSpace.define_finalizer(self, @client)
-    end
-
-    class ProducerClient
-      def initialize(native)
-        @native = native
-
-        # Start thread to poll client for delivery callbacks
-        @polling_thread = Thread.new do
-          loop do
-            Rdkafka::Bindings.rd_kafka_poll(native, 250)
-            # Exit thread if closing and the poll queue is empty
-            if Thread.current[:closing] && Rdkafka::Bindings.rd_kafka_outq_len(native) == 0
-              break
-            end
-          end
-        end
-        @polling_thread.abort_on_exception = true
-        @polling_thread[:closing] = false
-      end
-
-      def native
-        @native
-      end
-
-      def closed?
-        @native.nil?
-      end
-
-      def close(object_id=nil)
-        return unless @native
-
-        # Indicate to polling thread that we're closing
-        @polling_thread[:closing] = true
-        # Wait for the polling thread to finish up
-        @polling_thread.join
-
-        Rdkafka::Bindings.rd_kafka_destroy(@native)
-
-        @native = nil
-      end
-      alias call close
+      ObjectSpace.define_finalizer(self, client.finalizer)
     end
 
     # Set a callback that will be called every time a message is successfully produced.

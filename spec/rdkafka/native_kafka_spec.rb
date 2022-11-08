7b2519a3b1ca0c907172ad9f0146d6264952795c
Thijs Cadier <thijs@appsignal.com>
2022-11-08 17:27:36 +0100

    Lock access to the native kafka client
    Lock access from the admin and consumer class. This prevents rdkafka
    from segfauling if the native instance is accessed during close.
    
    We now destroy the kafka client in the polling thread after the polling
    loop has finished and any other calls are no longer in progress.

--- spec/rdkafka/native_kafka_spec.rb
+++ spec/rdkafka/native_kafka_spec.rb
@@ -8,7 +8,7 @@ describe Rdkafka::NativeKafka do
   let(:closing) { false }
   let(:thread) { double(Thread) }
 
-  subject(:client) { described_class.new(native, run_polling_thread: true) }
+  subject(:client) { described_class.new(native) }
 
   before do
     allow(Rdkafka::Bindings).to receive(:rd_kafka_poll).with(instance_of(FFI::Pointer), 250).and_call_original
@@ -41,42 +41,12 @@ describe Rdkafka::NativeKafka do
 
       client
     end
-
-    it "polls the native with default 250ms timeout" do
-      polling_loop_expects do
-        expect(Rdkafka::Bindings).to receive(:rd_kafka_poll).with(instance_of(FFI::Pointer), 250).at_least(:once)
-      end
-    end
-
-    it "check the out queue of native client" do
-      polling_loop_expects do
-        expect(Rdkafka::Bindings).to receive(:rd_kafka_outq_len).with(native).at_least(:once)
-      end
-    end
-
-    context "if not enabled" do
-      subject(:client) { described_class.new(native, run_polling_thread: false) }
-
-      it "is not created" do
-        expect(Thread).not_to receive(:new)
-
-        client
-      end
-    end
   end
 
-  def polling_loop_expects(&block)
-    Thread.current[:closing] = true # this forces the loop break with line #12
-
-    allow(Thread).to receive(:new).and_yield do |_|
-      block.call
-    end.and_return(thread)
-
-    client
-  end
-
-  it "exposes inner client" do
-    expect(client.inner).to eq(native)
+  it "exposes the inner client" do
+    client.with_inner do |inner|
+      expect(inner).to eq(native)
+    end
   end
 
   context "when client was not yet closed (`nil`)" do
@@ -85,12 +55,6 @@ describe Rdkafka::NativeKafka do
     end
 
     context "and attempt to close" do
-      it "calls the `destroy` binding" do
-        expect(Rdkafka::Bindings).to receive(:rd_kafka_destroy_flags).with(native, Rdkafka::Bindings::RD_KAFKA_DESTROY_F_IMMEDIATE)
-
-        client.close
-      end
-
       it "indicates to the polling thread that it is closing" do
         expect(thread).to receive(:[]=).with(:closing, true)
 
@@ -106,7 +70,6 @@ describe Rdkafka::NativeKafka do
       it "closes and unassign the native client" do
         client.close
 
-        expect(client.inner).to eq(nil)
         expect(client.closed?).to eq(true)
       end
     end
@@ -141,7 +104,6 @@ describe Rdkafka::NativeKafka do
       it "does not close and unassign the native client again" do
         client.close
 
-        expect(client.inner).to eq(nil)
         expect(client.closed?).to eq(true)
       end
     end

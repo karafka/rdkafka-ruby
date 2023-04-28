0d2d401610cf7b3c279325cf8bd40cf5d7295239
thijsc <thijs@appsignal.com>
2023-04-28 11:52:40 +0200

    Bump gem version to 0.13.0.beta.6

--- lib/rdkafka/version.rb
+++ lib/rdkafka/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Rdkafka
-  VERSION = "0.13.0.beta.5"
+  VERSION = "0.13.0.beta.6"
   LIBRDKAFKA_VERSION = "1.9.2"
   LIBRDKAFKA_SOURCE_SHA256 = "3fba157a9f80a0889c982acdd44608be8a46142270a389008b22d921be1198ad"
 end

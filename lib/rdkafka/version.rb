70e86510ef7903ca0ea041f79bae74c973184545
Thijs Cadier <thijs@appsignal.com>
2023-07-24 15:21:06 +0200

    Bump version to 0.13

--- lib/rdkafka/version.rb
+++ lib/rdkafka/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Rdkafka
-  VERSION = "0.13.0.beta.8"
+  VERSION = "0.13.0"
   LIBRDKAFKA_VERSION = "2.0.2"
   LIBRDKAFKA_SOURCE_SHA256 = "f321bcb1e015a34114c83cf1aa7b99ee260236aab096b85c003170c90a47ca9d"
 end

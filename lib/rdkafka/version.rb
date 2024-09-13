fb80a9403cd42043c95d656009b4465e3291b1ca
Maciej Mensfeld <maciej@mensfeld.pl>
2024-09-13 19:55:08 +0200

    Remove Ruby 3.0 support

--- lib/rdkafka/version.rb
+++ lib/rdkafka/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Rdkafka
-  VERSION = "0.18.1"
+  VERSION = "0.19.0"
   LIBRDKAFKA_VERSION = "2.5.0"
   LIBRDKAFKA_SOURCE_SHA256 = "3dc62de731fd516dfb1032861d9a580d4d0b5b0856beb0f185d06df8e6c26259"
 end

972f06fc5c61a2a82478d418a0f2ca67ca8a0e19
Maciej Mensfeld <maciej@mensfeld.pl>
2024-10-01 18:15:11 +0200

    expose rd_kafka_global_init

--- lib/rdkafka/version.rb
+++ lib/rdkafka/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Rdkafka
-  VERSION = "0.19.0"
+  VERSION = "0.19.1"
   LIBRDKAFKA_VERSION = "2.5.3"
   LIBRDKAFKA_SOURCE_SHA256 = "eaa1213fdddf9c43e28834d9a832d9dd732377d35121e42f875966305f52b8ff"
 end

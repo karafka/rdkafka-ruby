f6fc425829eaff5431b1d6a55baf60ac986203e8
Maciej Mensfeld <maciej@mensfeld.pl>
2025-07-01 17:28:27 +0200

    musl native extensions building + specs (no deploy) (#192)
    * musl build
    
    * musl poc
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * improve musl
    
    * add missing zstd
    
    * add missing zstd
    
    * add missing zstd
    
    * fix
    
    * fix
    
    * fix
    
    * add zlib for musl
    
    * remarks

--- lib/rdkafka/bindings.rb
+++ lib/rdkafka/bindings.rb
@@ -406,12 +406,15 @@ module Rdkafka
     def self.partitioner(str, partition_count, partitioner_name = "consistent_random")
       # Return RD_KAFKA_PARTITION_UA(unassigned partition) when partition count is nil/zero.
       return -1 unless partition_count&.nonzero?
+      # musl architecture crashes with empty string
+      return 0 if str.empty?
 
-      str_ptr = str.empty? ? FFI::MemoryPointer::NULL : FFI::MemoryPointer.from_string(str)
+      str_ptr = FFI::MemoryPointer.from_string(str)
       method_name = PARTITIONERS.fetch(partitioner_name) do
         raise Rdkafka::Config::ConfigError.new("Unknown partitioner: #{partitioner_name}")
       end
-      public_send(method_name, nil, str_ptr, str.size > 0 ? str.size : 1, partition_count, nil, nil)
+
+      public_send(method_name, nil, str_ptr, str.size, partition_count, nil, nil)
     end
 
     # Create Topics

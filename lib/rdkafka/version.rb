2ee1861d25e54274903a9257339e40ea22e23b1a
Maciej Mensfeld <maciej@mensfeld.pl>
2025-10-20 15:17:15 +0200

    v0.23.0.beta1 (#295)

--- lib/rdkafka/version.rb
+++ lib/rdkafka/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Rdkafka
-  VERSION = "0.23.0"
+  VERSION = "0.23.0.beta1"
   LIBRDKAFKA_VERSION = "2.12.0"
   LIBRDKAFKA_SOURCE_SHA256 = "1355d81091d13643aed140ba0fe62437c02d9434b44e90975aaefab84c2bf237"
 end

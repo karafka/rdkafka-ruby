d2b0daaf3c9df1d287d3832d01ec29bf6d02860d
Maciej Mensfeld <maciej@mensfeld.pl>
2026-05-08 17:36:14 +0200

    Bump librdkafka to 2.14.1 and version to 0.28.0
    - Update librdkafka from 2.14.0 to 2.14.1
    - Update rdkafka-ruby version from 0.27.0 to 0.28.0
    - Update SHA256 checksum for new tarball
    - Update CHANGELOG and README version table
    
    Closes #847

--- ext/build_common.sh
+++ ext/build_common.sh
@@ -19,7 +19,7 @@ readonly CYRUS_SASL_VERSION="2.1.28"
 readonly ZLIB_VERSION="1.3.1"
 readonly ZSTD_VERSION="1.5.7"
 readonly KRB5_VERSION="1.21.3"
-readonly LIBRDKAFKA_VERSION="2.14.0"
+readonly LIBRDKAFKA_VERSION="2.14.1"
 
 # SHA256 checksums for supply chain security
 # Update these when upgrading versions
@@ -29,7 +29,7 @@ declare -A CHECKSUMS=(
     ["zlib-1.3.1.tar.gz"]="9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23"
     ["zstd-${ZSTD_VERSION}.tar.gz"]="eb33e51f49a15e023950cd7825ca74a4a2b43db8354825ac24fc1b7ee09e6fa3"
     ["krb5-${KRB5_VERSION}.tar.gz"]="b7a4cd5ead67fb08b980b21abd150ff7217e85ea320c9ed0c6dadd304840ad35"
-    ["librdkafka-${LIBRDKAFKA_VERSION}.tar.gz"]="c05c03ef00a13a8463fac3e8918c04843c416f11ced58c889d806a88ca92cf99"
+    ["librdkafka-${LIBRDKAFKA_VERSION}.tar.gz"]="bb246e754dee3560e9b42bf4e844dc05de4b146a3cae937e36301ffacdc456e7"
 )
 
 # Colors for output

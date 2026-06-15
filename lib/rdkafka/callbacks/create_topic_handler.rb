580a8afac03321cec1fcd2dbed8d74cebc38e7cd
Maciej Mensfeld <maciej@mensfeld.pl>
2026-06-15 09:03:26 +0200

    Use class << self consistently in admin event handlers
    The handler classes mixed BaseHandler's class << self block with def self.call
    in the subclasses. Wrap every handler's call in class << self to match the
    base class. Behavior unchanged; specs and yard-lint pass.

--- lib/rdkafka/callbacks/create_topic_handler.rb
+++ lib/rdkafka/callbacks/create_topic_handler.rb
@@ -5,27 +5,29 @@ module Rdkafka
     # Handles `RD_KAFKA_EVENT_CREATETOPICS_RESULT` events
     # @private
     class CreateTopicHandler < BaseHandler
-      # Resolves the create-topic handle from its result event
-      # @param event_ptr [FFI::Pointer] pointer to the event
-      # @return [void]
-      def self.call(event_ptr)
-        create_topics_result = Rdkafka::Bindings.rd_kafka_event_CreateTopics_result(event_ptr)
+      class << self
+        # Resolves the create-topic handle from its result event
+        # @param event_ptr [FFI::Pointer] pointer to the event
+        # @return [void]
+        def call(event_ptr)
+          create_topics_result = Rdkafka::Bindings.rd_kafka_event_CreateTopics_result(event_ptr)
 
-        # Get the number of create topic results
-        pointer_to_size_t = FFI::MemoryPointer.new(:int32)
-        create_topic_result_array = Rdkafka::Bindings.rd_kafka_CreateTopics_result_topics(create_topics_result, pointer_to_size_t)
-        create_topic_results = TopicResult.create_topic_results_from_array(pointer_to_size_t.read_int, create_topic_result_array)
-        create_topic_handle_ptr = Rdkafka::Bindings.rd_kafka_event_opaque(event_ptr)
+          # Get the number of create topic results
+          pointer_to_size_t = FFI::MemoryPointer.new(:int32)
+          create_topic_result_array = Rdkafka::Bindings.rd_kafka_CreateTopics_result_topics(create_topics_result, pointer_to_size_t)
+          create_topic_results = TopicResult.create_topic_results_from_array(pointer_to_size_t.read_int, create_topic_result_array)
+          create_topic_handle_ptr = Rdkafka::Bindings.rd_kafka_event_opaque(event_ptr)
 
-        if create_topic_handle = Rdkafka::Admin::CreateTopicHandle.remove(create_topic_handle_ptr.address)
-          create_topic_handle[:response] = create_topic_results[0].result_error
-          create_topic_handle.result = Rdkafka::Admin::CreateTopicReport.new(
-            create_topic_results[0].error_string,
-            create_topic_results[0].result_name
-          )
-          create_topic_handle.broker_message = create_topic_handle.result.error_string
+          if create_topic_handle = Rdkafka::Admin::CreateTopicHandle.remove(create_topic_handle_ptr.address)
+            create_topic_handle[:response] = create_topic_results[0].result_error
+            create_topic_handle.result = Rdkafka::Admin::CreateTopicReport.new(
+              create_topic_results[0].error_string,
+              create_topic_results[0].result_name
+            )
+            create_topic_handle.broker_message = create_topic_handle.result.error_string
 
-          create_topic_handle.unlock
+            create_topic_handle.unlock
+          end
         end
       end
     end
--- lib/rdkafka/callbacks/describe_acl_handler.rb
+++ lib/rdkafka/callbacks/describe_acl_handler.rb
@@ -5,27 +5,29 @@ module Rdkafka
     # Handles `RD_KAFKA_EVENT_DESCRIBEACLS_RESULT` events
     # @private
     class DescribeAclHandler < BaseHandler
-      # Resolves the describe-ACL handle from its result event
-      # @param event_ptr [FFI::Pointer] pointer to the event
-      # @return [void]
-      def self.call(event_ptr)
-        describe_acl = DescribeAclResult.new(event_ptr)
-        describe_acl_handle_ptr = Rdkafka::Bindings.rd_kafka_event_opaque(event_ptr)
+      class << self
+        # Resolves the describe-ACL handle from its result event
+        # @param event_ptr [FFI::Pointer] pointer to the event
+        # @return [void]
+        def call(event_ptr)
+          describe_acl = DescribeAclResult.new(event_ptr)
+          describe_acl_handle_ptr = Rdkafka::Bindings.rd_kafka_event_opaque(event_ptr)
 
-        if describe_acl_handle = Rdkafka::Admin::DescribeAclHandle.remove(describe_acl_handle_ptr.address)
-          describe_acl_handle[:response] = describe_acl.result_error
-          describe_acl_handle.broker_message = read_event_string(describe_acl.error_string)
+          if describe_acl_handle = Rdkafka::Admin::DescribeAclHandle.remove(describe_acl_handle_ptr.address)
+            describe_acl_handle[:response] = describe_acl.result_error
+            describe_acl_handle.broker_message = read_event_string(describe_acl.error_string)
 
-          describe_acl_handle.result = if describe_acl.result_error == Rdkafka::Bindings::RD_KAFKA_RESP_ERR_NO_ERROR
-            Rdkafka::Admin::DescribeAclReport.new(
-              acls: describe_acl.matching_acls,
-              acls_count: describe_acl.matching_acls_count
-            )
-          else
-            Rdkafka::Admin::DescribeAclReport.new(acls: FFI::Pointer::NULL, acls_count: 0)
+            describe_acl_handle.result = if describe_acl.result_error == Rdkafka::Bindings::RD_KAFKA_RESP_ERR_NO_ERROR
+              Rdkafka::Admin::DescribeAclReport.new(
+                acls: describe_acl.matching_acls,
+                acls_count: describe_acl.matching_acls_count
+              )
+            else
+              Rdkafka::Admin::DescribeAclReport.new(acls: FFI::Pointer::NULL, acls_count: 0)
+            end
+
+            describe_acl_handle.unlock
           end
-
-          describe_acl_handle.unlock
         end
       end
     end
